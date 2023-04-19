f41d69e8545365726dfea8af7f1b01432421595c
1-aminadab <4ethiocihld@gmail.com>
2023-04-19 18:05:25 +0300

    send to specific room

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.use(cors())
-const http = require("http")
-const {Server} = require("socket.io")
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const server = http.createServer(app)
+app.use(cors());
+
+const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-  socket.on("send_message",(data)=>{
-    console.log(data);
-  })
-})
 
-server.listen(5000, ()=>{
-    console.log("Server is running at port 5000...");
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
+
